e59926b86a9c61360055cde60a2a73ae14447f91
Damen202 <babatundeogundare195@gmail.com>
2025-07-08 11:59:29 +0100

    The simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -8,6 +8,6 @@ rate = 0.05
 time = 3
 
 # calculate the simple interest
-The simple interest is = P * R * T
+interest = principal * rate * time
 # print answer
-print(The simple interest is)
+print(f"The simple interest is: {interest}")
