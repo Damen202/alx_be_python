1b417a62b3b5dda5e8e6cd081039833c62151884
Damen202 <babatundeogundare195@gmail.com>
2025-07-08 11:26:48 +0100

    The simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+I = "interest earned"
+P = "principal amount"
+R = "annual interest rate"
+T = "time"
+
+P = 1000
+R = 0.05
+T = 3
+
+# calculate the simple interest
+I = P * R * T
+
+# print answer
+print(I)
