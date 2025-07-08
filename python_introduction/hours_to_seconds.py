b901c18754ba61b2610ecc7c50a08c70e6f3e3a9
Damen202 <babatundeogundare195@gmail.com>
2025-07-08 12:41:09 +0100

    seconds in 2 hours

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+# Define numbers of hours
+hours = 2
+
+#seconds in an hour
+seconds = 3600
+
+#calculate for 2 hours
+seconds = 2 * 3600
+
+#print results
+print(f"{hours} hours(s) is {seconds} seconds")
