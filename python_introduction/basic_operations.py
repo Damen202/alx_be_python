f228690a880a8427afb5c0ef157204cca2d7d634
Damen202 <babatundeogundare195@gmail.com>
2025-07-15 18:08:14 +0100

    Basic arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -9,6 +9,6 @@ multiplication = number1 * number2
 
 # Print the results
 
-print(f"Addition of {number1} and {number2}")
-print(f"Subtraction of {number1} and {number2}")
-print(f"Multiplication of {number1} and {number2}")
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
