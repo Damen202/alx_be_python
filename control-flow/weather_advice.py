fde84b371ac861de19a8134356775cdda8abe6e6
Damen202 <babatundeogundare195@gmail.com>
2025-07-15 04:16:45 +0100

    New weather update

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+# Prompt the user for input
+current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+#cldnt_weather = input("sunny, rainy, cold.")
+
+if current_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+
+if current_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+
+if current_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
