a8e2eee20197250351d1802f343349b5fc227efa
Damen202 <babatundeogundare195@gmail.com>
2025-07-24 08:33:42 +0100

    A new system of arithmetic calculation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+    num1 = float(num1)
+    num2 = float(num2)
+    operation = str(operation)
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'divide':
+        if num2 == '0':
+            raise ValueError("number cannot be divided by zero.")
+        return num1 / num2
+    elif operation == 'multiply':
+        return num1 * num2
+    else:
+        raise ValueError(f"Invalid operation: {operation}")
