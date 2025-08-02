fec906c0dc4b9f4b8f8e215f0d5770922e802cce
Damen202 <babatundeogundare195@gmail.com>
2025-08-02 04:45:37 +0100

    library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,7 +4,24 @@ class Book:
         self.title = title
         self._is_checked_out = False
 
-   
+ #   def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+  #  def is_available(self):
+        return not self._is_checked_out
+
+ #   def __str__(self):
+ #       return f"'{self.title}' by {self.author}"
+
 
 class Library:
     def __init__(self):
