ddf5c79d9048c8c3f3b0d3bd0adbd1429f6c29bf
Damen202 <babatundeogundare195@gmail.com>
2025-08-08 20:24:25 +0100

    oop test

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,10 +7,13 @@ class Book:
 
     def __del__(self):
         self.title # Deleting title
-        print("Deleting 1984")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book( '{self.title}', '{self.author}', '{self.year}')"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+my_book = Book("1984", "George Orwell", 1949)
+print(repr(my_book))
