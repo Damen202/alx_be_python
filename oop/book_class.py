00609b80dd67b1afde7a977f8c4bfbcc4f07c705
Damen202 <babatundeogundare195@gmail.com>
2025-08-08 20:48:36 +0100

    oop test

--- oop/book_class.py
+++ oop/book_class.py
@@ -14,5 +14,4 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
 my_book = Book("1984", "George Orwell", 1949)
-print(my_book)
 print(repr(my_book))
--- oop/main.py
+++ oop/main.py
@@ -4,8 +4,8 @@ def main():
     # Creating an instance of Book
     my_book = Book("1984", "George Orwell", 1949)
 
-    # Demonstrating thr __str__ method
-    print(my_book) # Expected to use __str__
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
 
     # Deleting a book instance to trigger __del__
     del my_book
